2d43385a1838394cfbe2ab3bdac39e44b96b81e4
nimplay <nimrod7day@gmail.com>
2022-07-11 16:12:13 -0400

    delete files in the main branch

--- src/modules/clearButton.js
+++
@@ -1,8 +0,0 @@
-const mainContainer = document.querySelector('.container');
-const clearButton = () => {
-  const button = document.createElement('button');
-  button.className = 'clear-btn';
-  mainContainer.appendChild(button);
-  button.textContent = 'Clear all complete';
-};
-export default clearButton;
--- src/modules/get-task.js
+++
@@ -1,9 +0,0 @@
-const getData = () => {
-  const data = localStorage.getItem('Data');
-  if (data) {
-    return JSON.parse(data);
-  }
-  return [];
-};
-
-export default getData;
